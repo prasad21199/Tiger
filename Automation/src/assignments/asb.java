90975faa6ad658fac937422cba669ba46d32eaf9
smruti sarthak pasayat <smrutisarthakpasayat55555@gmail.com>
2024-12-20 09:51:14 +0530

    sarthak

--- Automation/src/assignments/asb.java
+++ Automation/src/assignments/asb.java
@@ -1,5 +1,32 @@
 package assignments;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class asb {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath("//textarea[@title='Search']")).sendKeys("Java");
+		List<WebElement> autosuggestion = driver.findElements(By.xpath("//span[contains(text(),'java')]"));
+		int count = autosuggestion.size();
+		System.out.println(count);
+		for(WebElement sugg:autosuggestion) {
+			System.out.println(sugg.getText());
+			
+		}
+		autosuggestion.get(1).click();
+		
+		driver.quit();
+		
+	}
 
 }
